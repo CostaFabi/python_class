0c39898f5a42427ab344c022d9467b70dfe93afa
fayecosta <fabinho.costa@gmail.com>
2019-01-29 12:22:03 -0200

    teste

---
+++ contagem.py
@@ -0,0 +1,16 @@
+
+import requests
+import json
+
+lista = json.loads(requests.get("https://bit.ly/2RXuXsD").text)
+
+dicionario = {}
+
+for x in lista:
+    if x not in dicionario:
+        dicionario[x] = 1
+
+    else:
+        dicionario[x] += 1
+    
+print(dicionario)
---
+++ test.py
@@ -0,0 +1,6 @@
+import math
+
+print(math.sqrt(121))
+print(math.log(math.e))
+print(math.cos(math.pi))
+print(math.fsum([.1, .1, .1, .1, .1, .1, .1, .1, .1, .1]))
